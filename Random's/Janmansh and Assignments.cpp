6806f65167ea7c77a7cf344b8e0956fde283c68e
Rohit Raj <kaneki422537@gmail.com>
2026-01-01 20:38:06 +0530

    Add few NEWcode

---
+++ Random's/Janmansh and Assignments.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x+3<=10)
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+    }
+    return 0;
+}
