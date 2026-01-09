2279bc465967b135f03856fa828ef9d3ad8f9e2c
Rohit Raj <kaneki422537@gmail.com>
2026-01-09 22:54:15 +0530

    update

---
+++ Random's/code.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if(a==d || b==d || c==d)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+}
